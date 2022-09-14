a9443c8c742a73ab9a501f30a6aed65817c64ee3
SmartCoder3000 <83769295+SmartCoder3000@users.noreply.github.com>
2022-09-13 22:18:11 -0400

    Create cloak.js

---
+++ cloak.js
@@ -0,0 +1,29 @@
+class TabCloak {
+  constructor(config) {
+    this.config = config;
+    this.originalTitle = document.title;
+  }
+
+  async title() {
+    var url = location.protocol + "//textance.herokuapp.com/title/" + this.config.url;
+    var response = await fetch(url);
+    var title = await response.text();
+    document.title = title;
+  }
+
+  favicon() {
+    var url = `${location.protocol}//www.google.com/s2/favicons?domain=${this.config.url}&sz=128`;
+    var link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+    link.href = url;
+  }
+
+  main() {
+    this.title();
+    this.favicon();
+  }
+}
